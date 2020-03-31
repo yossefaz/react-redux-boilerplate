45c7c7b8621f8a968888442bdfe774e3633782da
yossefaz <yossefaz@protonmail.com>
2020-03-31 12:28:48 +0300

    added redux-component sample

---
+++ src/components/redux-component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import { fetchData } from "../redux/actions";
+
+class ReduxComp extends React.Component {
+    state = {  }
+    componentDidMount() {
+        this.props.fetchData();
+    }
+    render() { 
+        return ( <div></div> );
+    }
+}
+ 
+export default connect(null, {fetchData})(ReduxComp);
---
+++ src/redux/actions/index.js
@@ -0,0 +1,5 @@
+export const fetchData = () => {
+    return {
+        type : 'FETCH_DATA'
+    }
+}
